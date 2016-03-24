1611fcc30e4d14bc119e83b341a1b7411d7b2505
James W Thorne <jameswthorne@gmail.com>
2016-03-24 11:05:09 -0500

    Fixed cinder spindle script

--- create_raid_cinder-spindle-only.sh
+++ create_raid_cinder-spindle-only.sh
@@ -7,8 +7,5 @@ SERIAL=$(/usr/sbin/hpssacli ctrl all show | grep sn: | cut -d: -f2 | sed 's@ @@g
 /sbin/dmsetup status
 # Logicaldrive 1 - OS RAID1 - drives 1I:1:1,1I:1:2
 /usr/sbin/hpssacli ctrl sn=${SERIAL} create type=ld raid=1 ss=256 drives=1I:1:1,1I:1:2 size=max ca=enable
-# Logicaldrive 2-9 - Cinder RAID0 - 1I:1:3 1I:1:4 1I:1:5 1I:1:6 1I:1:7 1I:1:8 2I:2:1 2I:2:2 2I:2:3 2I:2:4
-for DRIVE in 1I:1:3 1I:1:4 1I:1:5 1I:1:6 1I:1:7 1I:1:8 2I:2:1 2I:2:2 2I:2:3 2I:2:4
-do
-    /usr/sbin/hpssacli ctrl sn=${SERIAL} create type=ld raid=0 ss=256 drives=${DRIVE} size=max ca=enable
-done
+# Logicaldrive 2 - Cinder RAID10 - drives 1I:1:3,1I:1:4,1I:1:5,1I:1:6,1I:1:7,1I:1:8,2I:2:1,2I:2:2,2I:2:3,2I:2:4
+/usr/sbin/hpssacli ctrl sn=${SERIAL} create type=ld raid=1+0 ss=256 drives=1I:1:3,1I:1:4,1I:1:5,1I:1:6,1I:1:7,1I:1:8,2I:2:1,2I:2:2,2I:2:3,2I:2:4 size=max ca=enable
